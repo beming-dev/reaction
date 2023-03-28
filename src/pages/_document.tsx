b706c2e24bddd455434c55c7f60025315dceb407
beming-dev <mingfordev@gmail.com>
2023-03-28 22:55:49 +0900

    Add result box

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>반응속도 테스트</title>
+        <meta name="description" content="반응속도를 측정해보세요!" />
+        <meta name="keywords" content="반응속도, 테스트" />
+        <meta name="robots" content="index, follow" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
-import { Box, Button, Flex, Text, useToast } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Slide,
+  Text,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const toast = useToast();
-  const [top, setTop] = useState(0);
-  const [left, setLeft] = useState(0);
+  //140 ~ 1000 => 570
+  const rank = [150];
 
+  const toast = useToast();
   const [chance, setChance] = useState(5);
   const [visual, setVisual] = useState(false);
   const [timer, setTimer] = useState(3);
@@ -14,6 +22,8 @@ export default function Home() {
   const [reactionRate, setReactionRate] = useState<number>(0);
   const [timeoutIds, setTimeoutIds] = useState<any[]>([]);
   const [recordScore, setRecordScore] = useState<number[]>([]);
+
+  const { isOpen, onToggle } = useDisclosure();
 
   const init = () => {
     timeoutIds.forEach((id) => {
@@ -59,7 +69,10 @@ export default function Home() {
   };
 
   useEffect(() => {
-    if (chance === 0) init();
+    if (chance === 0) {
+      onToggle();
+      init();
+    }
   }, [chance]);
 
   useEffect(() => {
@@ -81,11 +94,20 @@ export default function Home() {
   };
 
   return (
-    <Flex w="100vw" h="100vh" flexDir="column" justify="center" align="center">
+    <Flex
+      w="100vw"
+      h="calc(100vh - 70px)"
+      flexDir="column"
+      justify="center"
+      align="center"
+      fontWeight="400"
+      position="relative"
+    >
+      <Text as="b">테스트는 총 5회 진행됩니다!</Text>
       <Box
         w="100%"
         h="300px"
-        mb="50px"
+        m="50px 0"
         position="relative"
         bgColor={
           !start ? "blackAlpha.100" : visual ? "#81C147" : "blackAlpha.100"
@@ -103,6 +125,7 @@ export default function Home() {
       <Flex flexWrap="wrap" align="center" justify="center" m="20px 0">
         {new Array(5).fill(0).map((v, i) => (
           <Flex
+            key={i}
             align="center"
             justify="center"
             w="100px"
@@ -118,6 +141,45 @@ export default function Home() {
       <Button bgColor="blackAlpha.200" onClick={onStart}>
         테스트 시작
       </Button>
+
+      <Slide direction="bottom" in={isOpen} style={{ zIndex: 10 }}>
+        <Flex
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          p="40px"
+          m="5vh"
+          h="50vh"
+          mt="4"
+          color="white"
+          bg="teal.500"
+          shadow="md"
+          fontSize="2xl"
+        >
+          <Text as="b">테스트 결과</Text>
+          <Text m="20px 0">
+            평균 속도:{" "}
+            {recordScore.reduce((a, b) => a + b, 0) / recordScore.length}ms
+          </Text>
+          <Text mb="20px">
+            상위{" "}
+            {Math.floor(
+              ((recordScore.reduce((a, b) => a + b, 0) / recordScore.length -
+                150) /
+                850) *
+                100
+            )}{" "}
+            %입니다!
+          </Text>
+          <Button
+            bgColor="transparent"
+            border="1px solid white"
+            onClick={onToggle}
+          >
+            닫기
+          </Button>
+        </Flex>
+      </Slide>
     </Flex>
   );
 }
