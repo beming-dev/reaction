0c058d1bd2f1431eb73e75e06f19819f68c1bcd8
beming-dev <mingfordev@gmail.com>
2023-03-29 13:46:40 +0900

    Add google ads

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,11 @@ export default function Document() {
         <meta name="description" content="반응속도를 측정해보세요!" />
         <meta name="keywords" content="반응속도, 테스트" />
         <meta name="robots" content="index, follow" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4911380706120456"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <body>
         <Main />
